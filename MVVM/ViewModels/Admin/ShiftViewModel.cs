d9282ac8fb6861b2558f96491400e2636fcbf3b2
ThisStateUser <flazersg183@gmail.com>
2024-04-03 12:19:48 +0400

    upd

--- MVVM/ViewModels/Admin/ShiftViewModel.cs
+++ MVVM/ViewModels/Admin/ShiftViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Cashbox.Core;
+using Cashbox.Core.Commands;
 using Cashbox.MVVM.ViewModels.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Cashbox.MVVM.ViewModels.Admin
 {
@@ -19,6 +21,27 @@ namespace Cashbox.MVVM.ViewModels.Admin
             get => _selectedOrder;
             set => Set(ref _selectedOrder, value);
         }
+
+        //private OrderViewModel? _selectedOrder;
+        //public OrderViewModel? SelectedOrder
+        //{
+        //    get => _selectedOrder;
+        //    set => Set(ref _selectedOrder, value);
+        //}
         #endregion
+
+        #region Commands
+        public RelayCommand RemoveProductCommand { get; set; }
+        private bool CanRemoveProductCommandExecute(object p) => true;
+        private async void OnRemoveProductCommandExecuted(object p)
+        {
+           
+        }
+        #endregion
+
+        public ShiftViewModel()
+        {
+                
+        }
     }
 }
